9106e9431f57ac8d54087619a958c0d60f4a9303
uo271314 <71763950+uo271314@users.noreply.github.com>
2020-11-11 15:31:39 +0100

    Add files via upload

---
+++ JavaScript(Bloque3)/Ejercicio-01/Titulo2.js
@@ -0,0 +1,4 @@
+// Llama método -> escribe en document (Objeto) el nombre de la titulacion en un encabezada de nivel 2.
+document.write("<h2>");
+document.write(asignatura.nombreTitulacion);
+document.write("</h2>");
---
+++ JavaScript(Bloque3)/Ejercicio-01/Titulo3.js
@@ -0,0 +1,4 @@
+// Llama método -> escribe en document (Objeto) el nombre del centro en un encabezado de nivel 3.
+document.write("<h3>");
+document.write(asignatura.nombreCentro);
+document.write("</h3>");
